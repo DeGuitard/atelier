f7dd503c5f4b4aaacf9cb010906203b33340a00a
Vianney Dupoy de Guitard <wired@n4v1.com>
2018-03-23 20:35:23 +0100

    Fixed missing trailing separator on static resources path.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var app = express();
 
-app.use(express.static(__dirname + '../client/public'));
+app.use(express.static(__dirname + '/../client/public'));
 app.use(expressLayouts);
 
 app.set('views', __dirname + '/../client/views');
